218e9a1ea7c56601de767541f80516f2ae21f1d3
nhut0902-pr <lamminhnhut09022011@gmail.com>
2025-08-25 11:01:53 +0700

    Delete webapp directory

--- webapp/db.py
+++
@@ -1,9 +0,0 @@
-# webapp/db.py
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-import os
-
-DB_URL = os.getenv("DATABASE_URL", "sqlite:///./app.db")
-engine = create_engine(DB_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
-Base = declarative_base()
